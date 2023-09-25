5abdb76d53f6232626380e7d10f42e62b5358c78
IreneAragon <irenearagongomez@gmail.com>
2023-09-25 19:57:14 +0200

    pagination added

--- src/pods/character-collection/character-collection.component.tsx
+++ src/pods/character-collection/character-collection.component.tsx
@@ -12,16 +12,35 @@ export const CharacterCollectionComponent: React.FunctionComponent<Props> = (
   props
 ) => {
   const { characterCollection, onDetails } = props;
+  const itemsPerPage = 5; 
+  const [currentPage, setCurrentPage] = React.useState(1);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = characterCollection.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
 
   return (
     <div className={classes.root}>
       <ul className={classes.list}>
-        {characterCollection.map((character) => (
+        {currentItems.map((character) => (
           <li key={character.id}>
             <CharacterCard character={character} onDetails={onDetails}/>
           </li>
         ))}
       </ul>
+      <div className={classes.pagination}>
+        <ul className={classes.paginationList}>
+          {Array.from({ length: Math.ceil(characterCollection.length / itemsPerPage) }, (_, index) => (
+            <li key={index} onClick={() => paginate(index + 1)} className={currentPage === index + 1 ? classes.activePage : classes.page}>
+              {index + 1}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
--- src/pods/character-collection/character-collection.styles.ts
+++ src/pods/character-collection/character-collection.styles.ts
@@ -24,3 +24,40 @@ export const list = css`
     grid-template-columns: repeat(3, 1fr);
   }
 `;
+
+export const pagination = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 2rem;
+`;
+
+export const paginationList = css`
+  list-style: none;
+  padding: 0;
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+  
+  // border: 1px solid red;
+`;
+
+export const activePage = css`
+  background-color: #1976d2;
+  color: white;
+  padding: 0.5rem 1rem;
+  border-radius: 50%;
+  cursor: pointer;
+  
+`;
+
+export const page = css`
+  // background-color: #1976d2;
+  // color: white;
+  // padding: 0.5rem 1rem;
+  // border-radius: 50%;
+  // cursor: pointer;
+  // border: 1px solid red;
+  padding: 0 1rem;
+  cursor: pointer;
+`;
